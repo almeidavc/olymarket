4483fd36f46ac206f94c53a6597c487bf690b1a5
Vicente Almeida <vicentecostaalmeida@gmail.com>
2023-04-27 13:10:02 +0200

    tab navigator

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -1,10 +1,10 @@
-import { NativeNavigation } from 'app/navigation/native'
+import { TabNavigation } from 'app/navigation/native'
 import { Provider } from 'app/provider'
 
 export default function App() {
   return (
     <Provider>
-      <NativeNavigation />
+      <TabNavigation />
     </Provider>
   )
 }
--- packages/app/navigation/native/index.tsx
+++ packages/app/navigation/native/index.tsx
@@ -1,32 +1,57 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import { HomeScreen } from '../../features/home/screen'
-import { UserDetailScreen } from '../../features/user/detail-screen'
+import { UserDetailScreen } from '../../features/post/detail-screen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { FontAwesome5 } from '@expo/vector-icons'
 
-const Stack = createNativeStackNavigator<{
-  home: undefined
-  'user-detail': {
-    id: string
-  }
-}>()
+// const Stack = createNativeStackNavigator<{
+//   home: undefined
+//   'user-detail': {
+//     id: string
+//   }
+// }>()
 
-export function NativeNavigation() {
+// export function NativeNavigation() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen
+//         name="home"
+//         component={HomeScreen}
+//         options={{
+//           title: 'Home',
+//         }}
+//       />
+//       <Stack.Screen
+//         name="user-detail"
+//         component={UserDetailScreen}
+//         options={{
+//           title: 'User',
+//         }}
+//       />
+//     </Stack.Navigator>
+//   )
+// }
+
+const tabIcons = {
+  Home: 'home',
+  Post: 'plus',
+}
+
+const Tab = createBottomTabNavigator()
+
+export function TabNavigation() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen
-        name="home"
-        component={HomeScreen}
-        options={{
-          title: 'Home',
-        }}
-      />
-      <Stack.Screen
-        name="user-detail"
-        component={UserDetailScreen}
-        options={{
-          title: 'User',
-        }}
-      />
-    </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => (
+          <FontAwesome5 name={tabIcons[route.name]} size={size} color={color} />
+        ),
+      })}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Post" component={UserDetailScreen} />
+    </Tab.Navigator>
   )
 }
