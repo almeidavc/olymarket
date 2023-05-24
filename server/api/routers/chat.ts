4e58743bdaa5d48bed1dec28b374f1d8de236c49
Vicente Almeida <vicentecostaalmeida@gmail.com>
2023-05-24 12:01:51 +0200

    format code in chat router

--- server/api/routers/chat.ts
+++ server/api/routers/chat.ts
@@ -34,118 +34,126 @@ function parseChat(conversation: CompleteConversation, me: string) {
   }
 }
 
-export const chatRouter = router({
-  find: protectedProcedure
-    .input(z.object({ partnerId: z.string(), postId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const conversation = await ctx.prisma.conversation.findFirst({
-        where: {
-          postId: input.postId,
-          participants: {
-            every: {
-              id: { in: [ctx.auth.userId, input.partnerId] },
-            },
-          },
-        },
-        include: {
-          participants: true,
-          messages: {
-            orderBy: {
-              createdAt: 'desc',
-            },
-          },
-          post: true,
-        },
-      })
-      return {
-        conversation,
-      }
-    }),
-  getById: protectedProcedure
-    .input(z.object({ chatId: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const conversation = await ctx.prisma.conversation.findUnique({
-        where: {
-          id: input.chatId,
-        },
-        include: {
-          participants: true,
-          post: true,
-          messages: {
-            orderBy: {
-              createdAt: 'desc',
-            },
-          },
-        },
-      })
-      if (!conversation) {
-        throw new TRPCError({ code: 'NOT_FOUND' })
-      }
-      if (
-        ctx.auth.userId !== conversation?.participants[0]?.id &&
-        ctx.auth.userId !== conversation?.participants[1]?.id
-      ) {
-        throw new TRPCError({ code: 'UNAUTHORIZED' })
-      }
-      return parseChat(conversation, ctx.auth.userId)
-    }),
-  list: protectedProcedure.query(async ({ ctx }) => {
-    const conversations = await ctx.prisma.conversation.findMany({
+const getById = protectedProcedure
+  .input(z.object({ chatId: z.string() }))
+  .query(async ({ ctx, input }) => {
+    const conversation = await ctx.prisma.conversation.findUnique({
       where: {
-        participants: {
-          some: {
-            id: ctx.auth.userId,
+        id: input.chatId,
+      },
+      include: {
+        participants: true,
+        post: true,
+        messages: {
+          orderBy: {
+            createdAt: 'desc',
           },
         },
-        messages: {
-          some: {},
+      },
+    })
+    if (!conversation) {
+      throw new TRPCError({ code: 'NOT_FOUND' })
+    }
+    if (
+      ctx.auth.userId !== conversation?.participants[0]?.id &&
+      ctx.auth.userId !== conversation?.participants[1]?.id
+    ) {
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
+    }
+    return parseChat(conversation, ctx.auth.userId)
+  })
+
+const list = protectedProcedure.query(async ({ ctx }) => {
+  const conversations = await ctx.prisma.conversation.findMany({
+    where: {
+      participants: {
+        some: {
+          id: ctx.auth.userId,
+        },
+      },
+      messages: {
+        some: {},
+      },
+    },
+    include: {
+      post: true,
+      participants: true,
+      messages: {
+        orderBy: {
+          createdAt: 'desc',
+        },
+        take: 1,
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  })
+  return conversations.map((conversation) =>
+    parseChat(conversation, ctx.auth.userId)
+  )
+})
+
+const find = protectedProcedure
+  .input(z.object({ partnerId: z.string(), postId: z.string() }))
+  .query(async ({ ctx, input }) => {
+    const conversation = await ctx.prisma.conversation.findFirst({
+      where: {
+        postId: input.postId,
+        participants: {
+          every: {
+            id: { in: [ctx.auth.userId, input.partnerId] },
+          },
         },
       },
       include: {
-        post: true,
         participants: true,
         messages: {
           orderBy: {
             createdAt: 'desc',
           },
-          take: 1,
         },
-      },
-      orderBy: {
-        createdAt: 'desc',
+        post: true,
       },
     })
-    return conversations.map((conversation) =>
-      parseChat(conversation, ctx.auth.userId)
-    )
-  }),
-  findOrCreate: protectedProcedure
-    .input(z.object({ partnerId: z.string(), postId: z.string() }))
-    .mutation(async ({ ctx, input }) => {
-      let conversation = await ctx.prisma.conversation.findFirst({
-        where: {
-          postId: input.postId,
+    return {
+      conversation,
+    }
+  })
+
+const findOrCreate = protectedProcedure
+  .input(z.object({ partnerId: z.string(), postId: z.string() }))
+  .mutation(async ({ ctx, input }) => {
+    let conversation = await ctx.prisma.conversation.findFirst({
+      where: {
+        postId: input.postId,
+        participants: {
+          every: {
+            id: { in: [ctx.auth.userId, input.partnerId] },
+          },
+        },
+      },
+    })
+    if (!conversation) {
+      conversation = await ctx.prisma.conversation.create({
+        data: {
           participants: {
-            every: {
-              id: { in: [ctx.auth.userId, input.partnerId] },
+            connect: [{ id: ctx.auth.userId }, { id: input.partnerId }],
+          },
+          post: {
+            connect: {
+              id: input.postId,
             },
           },
         },
       })
-      if (!conversation) {
-        conversation = await ctx.prisma.conversation.create({
-          data: {
-            participants: {
-              connect: [{ id: ctx.auth.userId }, { id: input.partnerId }],
-            },
-            post: {
-              connect: {
-                id: input.postId,
-              },
-            },
-          },
-        })
-      }
-      return conversation
-    }),
+    }
+    return conversation
+  })
+
+export const chatRouter = router({
+  getById,
+  list,
+  find,
+  findOrCreate,
 })
