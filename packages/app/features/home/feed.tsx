b4352ce90bf66db1966f479b72587d247128693a
Vicente Almeida <vicentecostaalmeida@gmail.com>
2023-05-24 18:05:44 +0200

    pull down to refresh posts feed

--- packages/app/features/home/feed.tsx
+++ packages/app/features/home/feed.tsx
@@ -3,14 +3,18 @@ import { View } from 'app/design/core'
 import { FlatList } from 'react-native'
 import { trpc } from 'app/utils/trpc'
 import { PostCard } from '../post/post'
+import { RefreshControl } from 'react-native'
 
 export function FeedScreen() {
-  const { data: posts } = trpc.post.list.useQuery()
+  const { data: posts, refetch, isRefetching } = trpc.post.list.useQuery()
 
   return (
     <View className="p-4">
       <FlatList
         ListHeaderComponent={<H1>Olymarket</H1>}
+        refreshControl={
+          <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
+        }
         data={posts}
         keyExtractor={(post) => post.id}
         numColumns={2}
