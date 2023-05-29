5b9c77bbe183c1c3052dd575a0c038e366a18a45
Vicente Almeida <vicentecostaalmeida@gmail.com>
2023-05-29 17:58:10 +0200

    use SafeAreaView in feed

--- packages/app/features/home/feed.tsx
+++ packages/app/features/home/feed.tsx
@@ -1,6 +1,6 @@
 import { H1 } from 'app/design/typography'
 import { View } from 'app/design/core'
-import { FlatList } from 'react-native'
+import { FlatList, SafeAreaView } from 'react-native'
 import { trpc } from 'app/utils/trpc'
 import { PostCard } from '../post/post'
 import { RefreshControl } from 'react-native'
@@ -9,22 +9,26 @@ export function FeedScreen() {
   const { data: posts, refetch, isRefetching } = trpc.post.list.useQuery()
 
   return (
-    <View className="mx-4 mt-12">
-      <FlatList
-        ListHeaderComponent={<H1>Olymarket</H1>}
-        refreshControl={
-          <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
-        }
-        showsVerticalScrollIndicator={false}
-        data={posts}
-        keyExtractor={(post) => post.id}
-        numColumns={2}
-        renderItem={({ item: post, index }) => (
-          <View className={`w-1/2 py-1.5 ${index % 2 === 0 ? 'pr-1' : 'pl-1'}`}>
-            <PostCard post={post} />
-          </View>
-        )}
-      />
-    </View>
+    <SafeAreaView>
+      <View className="px-4">
+        <FlatList
+          ListHeaderComponent={<H1>Olymarket</H1>}
+          refreshControl={
+            <RefreshControl refreshing={isRefetching} onRefresh={refetch} />
+          }
+          showsVerticalScrollIndicator={false}
+          data={posts}
+          keyExtractor={(post) => post.id}
+          numColumns={2}
+          renderItem={({ item: post, index }) => (
+            <View
+              className={`w-1/2 py-1.5 ${index % 2 === 0 ? 'pr-1' : 'pl-1'}`}
+            >
+              <PostCard post={post} />
+            </View>
+          )}
+        />
+      </View>
+    </SafeAreaView>
   )
 }
