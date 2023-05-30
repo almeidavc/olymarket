ba1940f8bb04ec56b720a37c68a795036b7e15a1
Vicente Almeida <vicentecostaalmeida@gmail.com>
2023-05-30 10:07:44 +0200

    fix tag

--- packages/app/components/tag.tsx
+++ packages/app/components/tag.tsx
@@ -1,18 +1,31 @@
 import { Text } from 'app/design/typography'
+import { View } from 'app/design/core'
 
 export type Color = 'red' | 'green'
 
-const colorVariants: { [color in Color]: string } = {
-  red: 'bg-red-100 text-red-800',
-  green: 'bg-green-100 text-green-800',
+const borderColors: { [color in Color]: string } = {
+  red: 'border-red-400',
+  green: 'border-green-400',
+}
+
+const backgroundColors: { [color in Color]: string } = {
+  red: 'bg-red-100',
+  green: 'bg-green-100',
+}
+
+const textColors: { [color in Color]: string } = {
+  red: 'text-red-800',
+  green: 'text-green-800',
 }
 
 export function Tag({ label, color }: { label: string; color: Color }) {
   return (
-    <Text
-      className={`rounded-full px-2.5 py-0.5 text-xs font-medium ${colorVariants[color]}`}
+    <View
+      className={`rounded-full border px-2.5 py-0.5 ${backgroundColors[color]} ${borderColors[color]}`}
     >
-      {label}
-    </Text>
+      <Text className={`text-xs font-medium ${textColors[color]}`}>
+        {label}
+      </Text>
+    </View>
   )
 }
