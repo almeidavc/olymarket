37309d8c7c7f79c8f1258d14b3bd1c818dd85770
Vicente Almeida <vicentecostaalmeida@gmail.com>
2023-06-25 17:34:21 +0200

    setup elastic password auth

--- server/elastic.ts
+++ server/elastic.ts
@@ -2,4 +2,8 @@ import { Client } from '@elastic/elasticsearch'
 
 export const elastic = new Client({
   node: process.env.ELASTICSEARCH_NODE_URL,
+  auth: {
+    username: 'elastic',
+    password: process.env.ELASTIC_PASSWORD,
+  },
 })
